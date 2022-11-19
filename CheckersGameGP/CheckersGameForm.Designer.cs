2c9bfae89c322f0feed8a763fda5e70de69a7779
Ankush Govind <axg33960@ucmo.edu>
2022-11-18 22:11:55 -0600

    Add project files.

---
+++ CheckersGameGP/CheckersGameForm.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace CheckersGameGP
+{
+    partial class CheckersGameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.PlayerInfoPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.QuestionsLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.PlayerInfoPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gray;
+            this.panel1.Controls.Add(this.PlayerInfoPanel);
+            this.panel1.Location = new System.Drawing.Point(10, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(645, 595);
+            this.panel1.TabIndex = 0;
+            // 
+            // PlayerInfoPanel
+            // 
+            this.PlayerInfoPanel.BackColor = System.Drawing.Color.Black;
+            this.PlayerInfoPanel.Controls.Add(this.PlayButton);
+            this.PlayerInfoPanel.Controls.Add(this.label17);
+            this.PlayerInfoPanel.Controls.Add(this.label16);
+            this.PlayerInfoPanel.Controls.Add(this.label7);
+            this.PlayerInfoPanel.Controls.Add(this.label15);
+            this.PlayerInfoPanel.Controls.Add(this.label5);
+            this.PlayerInfoPanel.Controls.Add(this.label14);
+            this.PlayerInfoPanel.Controls.Add(this.label9);
+            this.PlayerInfoPanel.Controls.Add(this.label13);
+            this.PlayerInfoPanel.Controls.Add(this.label4);
+            this.PlayerInfoPanel.Controls.Add(this.label12);
+            this.PlayerInfoPanel.Controls.Add(this.label6);
+            this.PlayerInfoPanel.Controls.Add(this.label11);
+            this.PlayerInfoPanel.Controls.Add(this.label8);
+            this.PlayerInfoPanel.Controls.Add(this.label10);
+            this.PlayerInfoPanel.Controls.Add(this.label3);
+            this.PlayerInfoPanel.Controls.Add(this.label2);
+            this.PlayerInfoPanel.Controls.Add(this.label1);
+            this.PlayerInfoPanel.Location = new System.Drawing.Point(285, 55);
+            this.PlayerInfoPanel.Name = "PlayerInfoPanel";
+            this.PlayerInfoPanel.Size = new System.Drawing.Size(188, 105);
+            this.PlayerInfoPanel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(380, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(312, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(123, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 43);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(123, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 43);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "label1";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(123, 300);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 43);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "label1";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(123, 373);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 43);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "label1";
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Location = new System.Drawing.Point(265, 143);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 43);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "label1";
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Location = new System.Drawing.Point(265, 373);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(87, 43);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "label1";
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Location = new System.Drawing.Point(265, 220);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(87, 43);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "label1";
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label9.Location = new System.Drawing.Point(265, 300);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(87, 43);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "label1";
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label10.Location = new System.Drawing.Point(686, 143);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(87, 43);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "label1";
+            // 
+            // label11
+            // 
+            this.label11.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label11.Location = new System.Drawing.Point(686, 220);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(87, 43);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "label1";
+            // 
+            // label12
+            // 
+            this.label12.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label12.Location = new System.Drawing.Point(828, 220);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(87, 43);
+            this.label12.TabIndex = 0;
+            this.label12.Text = "label1";
+            // 
+            // label13
+            // 
+            this.label13.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label13.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label13.Location = new System.Drawing.Point(828, 143);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(87, 43);
+            this.label13.TabIndex = 0;
+            this.label13.Text = "label1";
+            // 
+            // label14
+            // 
+            this.label14.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label14.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label14.Location = new System.Drawing.Point(686, 300);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(87, 43);
+            this.label14.TabIndex = 0;
+            this.label14.Text = "label1";
+            // 
+            // label15
+            // 
+            this.label15.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label15.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label15.Location = new System.Drawing.Point(828, 300);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(87, 43);
+            this.label15.TabIndex = 0;
+            this.label15.Text = "label1";
+            // 
+            // label16
+            // 
+            this.label16.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label16.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label16.Location = new System.Drawing.Point(686, 373);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(87, 43);
+            this.label16.TabIndex = 0;
+            this.label16.Text = "label1";
+            // 
+            // label17
+            // 
+            this.label17.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label17.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label17.Location = new System.Drawing.Point(828, 373);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(87, 43);
+            this.label17.TabIndex = 0;
+            this.label17.Text = "label1";
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayButton.Location = new System.Drawing.Point(430, 473);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(180, 55);
+            this.PlayButton.TabIndex = 1;
+            this.PlayButton.Text = "Play";
+            this.PlayButton.UseVisualStyleBackColor = true;
+            this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
+            // QuestionsLabel
+            // 
+            this.QuestionsLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.QuestionsLabel.Location = new System.Drawing.Point(675, 189);
+            this.QuestionsLabel.Name = "QuestionsLabel";
+            this.QuestionsLabel.Size = new System.Drawing.Size(387, 77);
+            this.QuestionsLabel.TabIndex = 1;
+            // 
+            // CheckersGameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1074, 624);
+            this.Controls.Add(this.QuestionsLabel);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimizeBox = false;
+            this.Name = "CheckersGameForm";
+            this.ShowIcon = false;
+            this.Text = "CheckersForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CheckersGameForm_FormClosing);
+            this.Load += new System.EventHandler(this.CheckersGameForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.PlayerInfoPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel PlayerInfoPanel;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button PlayButton;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label QuestionsLabel;
+    }
+}
---
+++ CheckersGameGP/CheckersGameForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckersGameGP
+{
+    public partial class CheckersGameForm : Form
+    {
+        private static CheckersGameForm CheckersGameInstance;
+        int n;
+        PictureBox[,] pictureBox;
+        public CheckersGameForm()
+        {
+            InitializeComponent();
+        }
+
+        internal static CheckersGameForm CheckersGameFormInstance()
+        {
+            if (CheckersGameInstance == null)
+
+                CheckersGameInstance = new CheckersGameForm();
+            return CheckersGameInstance;
+
+        }
+
+
+        private void CheckersGameForm_Load(object sender, EventArgs e)
+        {
+            n = 8;
+            pictureBox = new PictureBox[n, n];
+            int left = 2, top = 2;           
+            Color[] colors = new Color[] {Color.White, Color.Black };
+            for(int i = 0; i < n; i++)
+            {
+                left = 2;               
+                if (i % 2 == 0){colors[0] = Color.White; colors[1] = Color.Black;}
+                else {colors[1] = Color.White; colors[0] = Color.Black;}
+
+                for(int j = 0; j < n; j++)
+                {
+                   
+                    pictureBox[i,j]=new PictureBox();
+                    pictureBox[i,j].Size = new Size(60,60);
+                    pictureBox[i,j].BackColor = colors[(j%2==0) ? 1:0];
+                    pictureBox[i,j].Location=new Point(left, top);
+                    left +=60;
+                    panel1.Controls.Add(pictureBox[i,j]);
+                }
+                top +=60;
+            }
+
+
+        }
+
+        private void CheckersGameForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CheckersGameInstance = null;
+        }
+
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            PlayerInfoPanel.Visible=false;
+        }
+    }
+}
